e35cc9137edb274356dae2047f024a88f55b79ef
franciscorichter <f.richter@rug.nl>
2018-07-01 10:09:58 +0200

    minor changes

--- R/Efunctions.R
+++ R/Efunctions.R
@@ -353,7 +353,7 @@ mcem.tree <- function(tree,p,parallel=TRUE){
   k = 1
   print("initializing mcem")
   pars = p$p
-  PARS = c(NULL,NULL,NULL)
+  PARS = pars
   H = c(NULL,NULL,NULL)
   Me = p$M
   while(abs(D)>tol){
@@ -595,5 +595,62 @@ sim.extinct2 <- function(tree,pars,model='dd',seed=0, adjustment=FALSE){
   n = c(2,2+cumsum(E)+cumsum(E-1))
   tree$n = n
   return(tree)
+}
+
+
+post.pro <-function(file){
+load(file)
+pars = DDD::dd_ML(brts = btdd, idparsopt = 1:3,soc=2,cond=0)
+MLE = p$MLE
+M = mcem$PARS
+
+it = NULL
+lambda = NULL
+mu = NULL
+K = NULL
+for( i in 1:10){
+  MM = MLE[[i]]
+  it = c(it,rep(i+1,10))
+  lambda = c(lambda,MM[,1])
+  mu = c(mu,MM[,2])
+  K = c(K,MM[,3])
+};
+col = rep('grey',length(lambda))
+SDl = rep(NaN,length(lambda))
+SDm = rep(NaN,length(mu))
+SDk = rep(NaN,length(K))
+
+it = c(it,1:dim(M)[1])
+lambda = c(lambda,M[,2])
+mu = c(mu,M[,3])
+K = c(K,M[,4])
+col = c(col,rep('blue',dim(M)[1]))
+
+MCEMc = data.frame(it=it,lambda=lambda,mu=mu,K=K,col=col)
+
+gamLambda = gam(lambda ~ s(it), data=MCEMc)
+gamMu = gam(mu ~ s(it), data=MCEMc)
+gamK = gam(K ~ s(it), data=MCEMc)
+
+hessL = c(rep(NaN,10),mcem$H[,1])
+SDl = c(SDl,sqrt(hessL+gamLambda$sig2))
+
+hessM = c(rep(NaN,10),mcem$H[,2])
+SDm = c(SDm,sqrt(hessM+gamMu$sig2))
+
+hessK = c(rep(NaN,10),mcem$H[,3])
+SDk = c(SDk,sqrt(hessK+gamK$sig2))
+
+MCEMc$SDl = SDl
+MCEMc$SDm = SDm
+MCEMc$SDk = SDk
+
+MCEM = rbind(MCEMc,data.frame(it=(-9:0),lambda=p$PM[,1],mu=p$PM[,2],K=p$PM[,3],col=rep('blue',10),SDl=rep(NaN,10),SDm=rep(NaN,10),SDk=rep(NaN,10)))
+
+gl=ggplot(MCEM) + geom_point(aes(it,lambda),colour=MCEM$col) + geom_errorbar(aes(x=it, y=lambda, ymin = lambda-1.96*SDl, ymax = lambda + 1.96*SDl), colour='darkgreen') + geom_hline(yintercept = pars$lambda) + ggtitle('Plethodon')+theme(axis.text=element_text(size=18),axis.title=element_text(size=16))+labs(x='EM iteration',y=expression(lambda[0]))
+gm=ggplot(MCEM) + geom_point(aes(it,mu),colour=MCEM$col) + geom_errorbar(aes(x=it, y=mu, ymin = mu-1.96*SDm, ymax = mu + 1.96*SDm), colour='darkgreen') + geom_hline(yintercept = pars$mu)+ ggtitle('Plethodon')+theme(axis.text=element_text(size=18),axis.title=element_text(size=16))+labs(x='EM iteration',y=expression(mu[0]))
+gk=ggplot(MCEM) + geom_point(aes(it,K),colour=MCEM$col) + geom_errorbar(aes(x=it, y=K, ymin = K-1.96*SDk, ymax = K + 1.96*SDk), colour='darkgreen') + geom_hline(yintercept = pars$K) + ggtitle('Plethodon')+theme(axis.text=element_text(size=18),axis.title=element_text(size=16))+labs(x='EM iteration',y=expression(K))
+
+return(list(gl=gl,gm=gm,gk=gk))
 }
 
