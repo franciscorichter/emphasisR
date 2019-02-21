653259bb13aa5776b6747c6ed703f4f4da635f5e
Francisco <franciscorichter@gmail.com>
2019-02-21 15:53:08 -0300

    Update mcem_ex1.R

--- inst/Shiny-examples/mcem_ex1.R
+++ inst/Shiny-examples/mcem_ex1.R
@@ -1,25 +1,33 @@
+rm(brts)
+
 ui <- fluidPage(
   sidebarLayout(position = "left",
                 sidebarPanel("Controls",
+                             textInput('vec1', 'Enter a vector (comma delimited) with branching times', "0,1,2"),
                              actionButton("gogobutt","Go"),
                              actionButton("stopbutt","Stop"),
                              actionButton("resetbutt","Reset"),
                              numericInput("ss", "Monte-Carlo sample size:", 10),
+                             #numericInput("brts", "brts:", c(0.1,0.2,3,4)),
                              numericInput("lambda", "Initial lambda:", 10)),
+  
                 mainPanel("Plot",
                           plotOutput("histplot"),
-                          plotOutput("mu"))
+                          plotOutput("mu"),
+                          plotOutput("K")
+                          )
   ))
 server <- function(input,output,session) {
   pars = c(10,0.1,5)
   rv <- reactiveValues(x=c(10,0.1,5),run=F)
-  
   autoInvalidate <- reactiveTimer(intervalMs=500,session)
   
   observe({
     autoInvalidate()
-    isolate({ if (rv$run) { rv$x <- rbind(rv$x,mcem_step(brts,pars,maxnumspec = 15,MC_ss = input$ss))
-    pars = rv$x[nrow(rv$x),]
+    isolate({ if (rv$run) { 
+      brts <- as.numeric(unlist(strsplit(input$vec1,",")))
+      rv$x <- rbind(rv$x,mcem_step(brts,pars,maxnumspec = 15,MC_ss = input$ss))
+      pars = rv$x[nrow(rv$x),]
     } })
   })
   
@@ -36,6 +44,10 @@ server <- function(input,output,session) {
     htit <- sprintf("Hist of %d rnorms",length(rv$x))
     plot(1:nrow(rv$x),rv$x[,2],type="l")
     ##hist(rv$x,col = "steelblue",main=htit,breaks=12)
+  })
+  output$K <- renderPlot({
+    htit <- sprintf("K",length(rv$x))
+    plot(1:nrow(rv$x),rv$x[,3],type="l")
   })
 }
 shinyApp(ui, server)
