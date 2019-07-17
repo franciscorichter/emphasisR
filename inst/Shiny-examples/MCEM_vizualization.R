2135db02d77427ed450e13ded335aef50816100c
Francisco <franciscorichter@gmail.com>
2019-07-17 00:09:19 -0400

    updating emphasis analytics app

--- inst/Shiny-examples/MCEM_vizualization.R
+++ inst/Shiny-examples/MCEM_vizualization.R
@@ -1,4 +1,7 @@
 library(shiny)
+library(ggplot2)
+rm(list = ls())
+
 
 # Define UI for dataset viewer app ----
 ui <- fluidPage(
@@ -13,13 +16,17 @@ ui <- fluidPage(
     sidebarPanel(
       
       fileInput("file1", "Choose RData File",
+                multiple = TRUE,
                 accept = ".RData"
       ),
       
       # Input: Selector for choosing dataset ----
-      selectInput(inputId = "dataset",
-                  label = "Choose parameter to see:",
-                  choices = c("par1", "par2", "par3")),
+      selectInput(inputId = "par",
+                  label = "Choose plot parameter:",
+                  choices = c("par1", "par2", "par3","par1VSpar3","eff.sizeVSpar1","oneline","lambdaHist")),
+      
+      uiOutput('columns1'),
+      uiOutput('columns2'),
       
       # Input: Numeric entry for number of obs to view ----
       numericInput(inputId = "obs",
@@ -28,7 +35,15 @@ ui <- fluidPage(
       
       numericInput(inputId = "burning",
                    label = "Number of observations to burn:",
-                   value = 10)
+                   value = 10),
+      
+      numericInput(inputId = "filter",
+                   label = "Filter by size:",
+                   value = 0),
+      
+      selectInput(inputId = "rep",
+                  label = "Choose plot replicant:",
+                  choices = c("all",1:2))
       
     ),
     
@@ -36,16 +51,18 @@ ui <- fluidPage(
     mainPanel(
       
       # Output: Formatted text for caption ----
-      plotOutput("lambda"),
+      plotOutput("parameter_estimation_general"),
       
       # Output: Verbatim text for data summary ----
-      verbatimTextOutput("summary"),
-      verbatimTextOutput("summary2"),
+      verbatimTextOutput("summary")#,
+      #verbatimTextOutput("summary2"),
+     # verbatimTextOutput("In")
       
       # Output: HTML table with requested number of observations ----
       #tableOutput("view")
-      plotOutput("lambda2"),
-      plotOutput("logfhat")
+      #plotOutput("sample_size"),
+      #plotOutput("lambda2"),
+      #plotOutput("logfhat")
       
     )
   )
@@ -65,13 +82,39 @@ server <- function(input, output) {
   
   DF <- reactive({
     inFile <- input$file1
-    load(inFile$datapath)
-    if(exists("MCEM_temp")){
-      DF = MCEM_temp
-    }else{
-      DF$em.iteration = DF$iteration
+    DF_temp = NULL
+    for(i in 1:nrow(inFile)){
+      load(inFile[[i, 'datapath']])
+      if(exists("MCEM_temp")){
+        DF = data.frame(iteration=MCEM_temp$em.iteration,par1=MCEM_temp$par1,par2=MCEM_temp$par2,par3=MCEM_temp$par3,E_time=MCEM_temp$E_time,Mtime=MCEM_temp$M_time,efective_sample_size=MCEM_temp$effective.size,h1=1/MCEM_temp$hessian.inv1,h2=1/MCEM_temp$hessian.inv2,h3=1/MCEM_temp$hessian.inv3)
+        rm(MCEM_temp)
+      }
+      if("pars3" %in% names(DF)){
+        names(DF)[names(DF)=="pars3"] <- "par3"
+      }
+      DF$rep = i
+      DF_temp = rbind(DF_temp,DF)
     }
-    return(DF)
+    DF_temp$rep = as.character(DF_temp$rep)
+    return(DF_temp)
+  })
+  
+  In <- reactive({
+    inFile <- input$file1
+    load(inFile$datapath)
+    return(input)
+  })
+  
+  output$columns1 = renderUI({
+    selectInput(inputId = "par1",
+                label = "Choose x plot parameter:",
+                choices=names(DF()))
+  })
+  
+  output$columns2 = renderUI({
+    selectInput(inputId = "par2",
+                label = "Choose y plot parameter:",
+                choices=names(DF()))
   })
   
   
@@ -104,7 +147,12 @@ server <- function(input, output) {
   output$summary2 <- renderPrint({
     df = DF()
     df = df[input$burning:nrow(df),]
+    df = df[df$efective_sample_size > input$filter,]
     summary(log(df$fhat))
+  })
+  
+  output$In <- renderPrint({
+    In()
   })
   
   # Show the first "n" observations ----
@@ -115,13 +163,68 @@ server <- function(input, output) {
     head(datasetInput(), n = input$obs)
   })
   
-  
-  output$lambda <-renderPlot({
+  output$parameter_estimation <- renderPlot({
     df = DF()
-    lambda.est = mean(df$par1[input$burning:nrow(df)])
+    df = df[input$obs:nrow(df),]
+    df = df[df$efective_sample_size >= input$filter,]
+    if(input$par == "par1"){
+      par_est = mean(df$par1[input$burning:nrow(df)])
+      plot_par_est = ggplot(df)+geom_line(aes(x=iteration,y=par1, colour=rep))+ggtitle("lambda Estimation",subtitle = paste("Current estimation:",par_est))+geom_hline(yintercept = par_est,colour="red")
+    }
+    if(input$par == "par2"){
+      par_est = mean(df$par2[input$burning:nrow(df)])
+      plot_par_est = ggplot(df)+geom_line(aes(x=iteration,y=par2))+ggtitle("beta Estimation",subtitle = paste("Current estimation:",par_est))+geom_hline(yintercept = par_est,colour="red")
+    }
+    if(input$par == "par3"){
+      par_est = mean(df$par3[input$burning:nrow(df)])
+      plot_par_est = ggplot(df)+geom_line(aes(x=iteration,y=par3))+ggtitle("mu Estimation",subtitle = paste("Current estimation:",par_est))+geom_hline(yintercept = par_est,colour="red")
+    }
+    if(input$par == "par1VSpar3"){
+      plot_par_est = ggplot(df)+geom_path(aes(x=par1,y=par3,colour=rep))+ggtitle("lambda vs mu Estimation")#+geom_hline(yintercept = par_est,colour="red")
+    }
+    if(input$par == "eff.sizeVSpar1"){
+      plot_par_est = ggplot(df)+geom_path(aes(x=efective_sample_size,y=par1,colour=rep))+ggtitle("lambda vs mu Estimation")#+geom_hline(yintercept = par_est,colour="red")
+    }
+    if(input$par == "oneline"){
+      df2 = df[order(df$efective_sample_size),]
+      df2$iteration = 1:nrow(df2)
+      plot_par_est = ggplot(df2) + geom_line(aes(x=iteration,y=par1))
+    }
+    if(input$par == "lambdaHist"){
+      plot_par_est = ggplot(df)+geom_histogram(aes(x=par1))
+    }
+    if(input$par == "samplesize"){
+      plot_par_est = ggplot(df)+geom_line(aes(x=iteration,y=efective_sample_size))+ggtitle("Effective sample size")
+    }
+    
+    
+    
+    if("mc.samplesize" %in% names(df)){
+      change.ss = which(diff(df$mc.samplesize)>0)
+    }else{
+      change.ss = NULL
+    }
+    if(sum(change.ss)>0){
+      plot_par_est = plot_par_est + geom_vline(xintercept = change.ss, colour="darkgreen",linetype="dotted")
+    }
+    plot_par_est
+  })
+  
+  output$parameter_estimation_general <- renderPlot({
+    df = DF()
+    df = df[input$obs:nrow(df),]
+    df = df[df$efective_sample_size >= input$filter,]
+   # df2 = data.frame()
+    plot_par_est = ggplot(df,aes(colour=rep))+geom_point(aes_string(x=input$par1,y=input$par2))#+ggtitle("lambda Estimation",subtitle = paste("Current estimation:",par_est))+geom_hline(yintercept = par_est,colour="red")
+    plot_par_est
+  })
+  
+  
+  output$sample_size <-renderPlot({
+    df = DF()
     df = df[input$obs:nrow(df),]
     change.ss = which(diff(df$mc.samplesize)>0)
-    plot.lambda = ggplot(df)+geom_line(aes(x=em.iteration,y=par1))+ggtitle("Lambda Estimation",subtitle = paste("Current estimation:",lambda.est))+geom_hline(yintercept = lambda.est,colour="red")
+    plot.lambda = ggplot(df)+geom_line(aes(x=iteration,y=efective_sample_size))+ggtitle("Effective sample size")#+geom_hline(yintercept = lambda.est,colour="red")
     if(sum(change.ss)>0){
       plot.lambda = plot.lambda + geom_vline(xintercept = change.ss, colour="darkgreen",linetype="dotted")
     }
@@ -148,8 +251,8 @@ server <- function(input, output) {
     if(nrow(rv$MCEM)>5){ 
       mu.est = rv$MCEM$par2[length(rv$MCEM$par2)]
       MCEM = rv$MCEM[input$charts:length(rv$MCEM$par3),]
-      plot.mu = ggplot(MCEM) + geom_line(aes(em.iteration,par2)) + ggtitle(label=paste("Last estimation:  ",mu.est)) + ylab(expression(lambda1)) + xlab("EM iteration")
-      if(input$CI & nrow(rv$MCEM)>12) plot.mu = plot.mu + geom_errorbar(aes(x=em.iteration, y=par2, ymin = par2-1.96*sdm, ymax = par2 + 1.96*sdm), colour='darkgreen') 
+      plot.mu = ggplot(MCEM) + geom_line(aes(iteration,par2)) + ggtitle(label=paste("Last estimation:  ",mu.est)) + ylab(expression(lambda1)) + xlab("EM iteration")
+      if(input$CI & nrow(rv$MCEM)>12) plot.mu = plot.mu + geom_errorbar(aes(x=iteration, y=par2, ymin = par2-1.96*sdm, ymax = par2 + 1.96*sdm), colour='darkgreen') 
       change.ss = which(diff(rv$MCEM$mc.samplesize)>0)
       if(sum(change.ss)>0){
         plot.mu = plot.mu + geom_vline(xintercept = change.ss, colour="darkgreen",linetype="dotted")
@@ -162,7 +265,7 @@ server <- function(input, output) {
     if(nrow(rv$MCEM)>5){ 
       K.est = mean(rv$MCEM$par3[input$charts:length(rv$MCEM$par3)])
       MCEM = rv$MCEM[input$charts:length(rv$MCEM$par3),]
-      K.plot = ggplot(MCEM) + geom_line(aes(em.iteration,par3)) + ggtitle(label=paste("Last estimation:  ",K.est)) + ylab(expression(mu)) + xlab("EM iteration")
+      K.plot = ggplot(MCEM) + geom_line(aes(iteration,par3)) + ggtitle(label=paste("Last estimation:  ",K.est)) + ylab(expression(mu)) + xlab("EM iteration")
       change.ss = which(diff(rv$MCEM$mc.samplesize)>0)
       if(sum(change.ss)>0){
         K.plot = K.plot + geom_vline(xintercept = change.ss, colour="darkgreen",linetype="dotted")
@@ -174,9 +277,9 @@ server <- function(input, output) {
   output$fhat <- renderPlot({
     if(nrow(rv$MCEM)>5){ 
       MCEM = rv$MCEM[input$charts:length(rv$MCEM$par3),]
-      fhat.plot = ggplot(MCEM) + geom_line(aes(em.iteration,log(fhat))) # + ggtitle(label=paste("Last estimation:  ",mean(rv$mcem_it$K[input$charts:length(rv$mcem_it$K)])),subtitle =   paste("number of last iterations to consider: ", length(rv$mcem_it$K)-input$charts)) 
+      fhat.plot = ggplot(MCEM) + geom_line(aes(iteration,log(fhat))) # + ggtitle(label=paste("Last estimation:  ",mean(rv$mcem_it$K[input$charts:length(rv$mcem_it$K)])),subtitle =   paste("number of last iterations to consider: ", length(rv$mcem_it$K)-input$charts)) 
       #  if(input$ddd) fhat.plot = fhat.plot + geom_point(aes(em.iteration,ftrue))
-      if(input$CI) fhat.plot = fhat.plot + geom_line(aes(em.iteration,log(rv$MCEM$fhat+1.96*rv$MCEM$fhat.se)),col="red") + geom_line(aes(em.iteration,log(rv$MCEM$fhat-1.96*rv$MCEM$fhat.se)),col="red")#+ geom_errorbar(aes(x=it, y=K, ymin = K-1.96*sdk, ymax = K + 1.96*sdk), colour='darkgreen')
+      if(input$CI) fhat.plot = fhat.plot + geom_line(aes(iteration,log(rv$MCEM$fhat+1.96*rv$MCEM$fhat.se)),col="red") + geom_line(aes(iteration,log(rv$MCEM$fhat-1.96*rv$MCEM$fhat.se)),col="red")#+ geom_errorbar(aes(x=it, y=K, ymin = K-1.96*sdk, ymax = K + 1.96*sdk), colour='darkgreen')
       change.ss = which(diff(rv$MCEM$mc.samplesize)>0)
       if(sum(change.ss)>0){
         fhat.plot = fhat.plot + geom_vline(xintercept = change.ss, colour="darkgreen",linetype="dotted")
