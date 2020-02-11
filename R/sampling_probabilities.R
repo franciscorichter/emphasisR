0246ae6aebcbb1ec5bd17b9867289ac45c4d0576
franciscorichter <franciscorichter@gmail.com>
2020-02-11 02:26:00 -0300

    back to numerical

--- R/sampling_probabilities.R
+++ R/sampling_probabilities.R
@@ -1,6 +1,29 @@
 # augmentaion (sampling) probability 
 
 log_sampling_prob_nh <- function(df,pars,model="dd",soc,...){
+  to = top = head(df$to,-1)
+  to[to==2] = 1
+  initspec=soc
+  N = c(initspec,initspec+cumsum(to)+cumsum(to-1))
+  brts_i = df$brts
+  brts_im1 = c(0,df$brts[-nrow(df)])
+  missing_speciations = (df$to == 1)
+  nb = N[missing_speciations]
+  No = c(1,1+cumsum(top==2))[missing_speciations]
+  Ne = c(0,cumsum(top==1)-cumsum(top==0))[missing_speciations]
+  lambda_b = sapply(df$brts[df$to==1]-0.000000001,speciation_rate,tree = df,pars = pars,model = model,soc)
+  if(length(lambda_b)==0) lambda_b = 1
+  text = df$t_ext[df$to==1]-df$brts[df$to==1]
+  mu = max(0,pars[1])
+  inte = vector(mode = "numeric",length = length(brts_i))
+  for(i in 1:length(brts_i)){
+    inte[i] = intensity(x=brts_i[i],tree = df,model = model,time0 = brts_im1[i],pars = pars,soc,...)
+  }
+  logg = -sum(inte)+sum(log(nb)+log(mu))-sum(mu*text)+sum(log(lambda_b))-sum(log(2*No+Ne))
+  return(logg)
+}
+
+log_sampling_prob_nh_old <- function(df,pars,model="dd",soc,...){
   if(is.null(df$t_ext)) df$t_ext  = df$bte
   b = max(df$brts)
   to = top = head(df$to,-1)
@@ -26,13 +49,9 @@ log_sampling_prob_nh <- function(df,pars,model="dd",soc,...){
 }
 
 sampling_prob <- function(df,pars,model,soc){
-  if(is.null(df$t_ext)) df$t_ext  = df$bte
-  b = max(df$brts)
-  
   to = top = head(df$to,-1)
   to[to==2] = 1
-  initspec=soc
-  N = c(initspec,initspec+cumsum(to)+cumsum(to-1))
+  N = c(soc,soc+cumsum(to)+cumsum(to-1))
   
   brts_i = df$brts
   brts_im1 = c(0,df$brts[-nrow(df)])
