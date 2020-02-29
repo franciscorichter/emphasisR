50bd25e227e0358080a0623041cb68f39d5f2da5
franciscorichter <franciscorichter@gmail.com>
2020-02-29 01:43:16 -0300

    up

---
+++ R/sampling_probabilities.R
@@ -0,0 +1,119 @@
+# augmentaion (sampling) probability 
+
+sampling_prob <- function(tree,pars,model,soc,numerical=FALSE){
+  to = top = head(tree$to,-1)
+  to[to==2] = 1
+  N = c(soc,soc+cumsum(to)+cumsum(to-1))
+  
+  brts_i = tree$brts
+  brts_im1 = c(0,tree$brts[-nrow(tree)])
+  
+  missing_speciations = (tree$to == 1)
+  nb = N[missing_speciations]
+  No = c(soc,soc+cumsum(top==2))[missing_speciations]
+  Ne = c(0,cumsum(top==1)-cumsum(top==0))[missing_speciations]
+  
+  lambda_b = sapply(tree$brts[tree$to==1]-0.000000001,speciation_rate,tree = tree,pars = pars,model = model,soc=soc)
+  if(length(lambda_b)==0) lambda_b = 1
+  text = tree$t_ext[tree$to==1]-tree$brts[tree$to==1]
+  mu = max(0,pars[1])
+  if(numerical){
+    inte = intensity.numerical(tree = tree,pars = pars,model=model)
+  }else{
+    intensity.temp = get(paste0("intensity.", model))
+    inte = intensity.temp(tree=tree,pars=pars)
+  }
+  
+  logg = -sum(inte)+sum(log(nb)+log(mu))-sum(mu*text)+sum(log(lambda_b))-sum(log(2*No+Ne))
+  return(logg)
+}
+
+
+
+intensity.rpd1 <- function(tree, pars){
+  
+  mu = max(0,pars[1])
+  n = tree$n
+  lambda = pmax(0,pars[2] + pars[3] * n)
+  wt = diff(c(0,tree$brts))
+  brts_i = tree$brts
+  brts_im1 = c(0,tree$brts[-nrow(tree)])
+  sigma_over_tree = n*(lambda)*(wt - (exp(-mu*max(tree$brts))/mu)*(exp(mu*brts_i)-exp(mu*brts_im1)) )
+  
+  return(sigma_over_tree)
+}
+
+
+
+intensity.numerical <- function(tree, pars, model){
+  nh_rate <- function(x){
+    sum_speciation_rate(x=x,tree = tree,pars = pars,model = model,soc=tree$n[1])*(1-exp(-(max(tree$brts)-x)*pars[1]))
+  }
+  brts_i = tree$brts
+  brts_im1 = c(0,brts_i[-length(brts_i)])
+  inte = vector(mode="numeric",length = length(brts_i))
+  for(i in 1:length(brts_i)){
+    inte[i] = pracma:::quad(f = Vectorize(nh_rate),xa = brts_im1[i],xb = brts_i[i]-0.00000000001)
+  }
+  return(inte)
+}
+
+
+### intensity rpd5
+
+intensity.rpd5 <- function(tree,pars){
+  n = tree$n; Pt = c(0,tree$pd)
+  c2 = pars[4]; c3 = exp(-pars[1]*max(tree$brts)); c4 = pars[1]
+  c1 = pars[2] + pars[3]*n + pars[4]*((Pt[-nrow(tree)]-n*c(0,tree$brts[-length(tree$brts)]))/n)
+  if(c2==0){
+    roots = rep(Inf,times=length(c1))
+  }else{
+    roots = -c1/c2
+  }
+  brts_i = tree$brts
+  brts_im1 = c(0,brts_i[-length(brts_i)])
+  inte = vector(mode="numeric",length = length(brts_i))
+  for(i in 1:nrow(tree)){
+    if((roots[i] > brts_im1[i]) & (roots[i] < brts_i[i])  ){
+      if((pars[2] + pars[3] * n[i] + pars[4] *(Pt[i]/n[i]))<0){
+        inte[i] = (ind.rpd5(x=brts_i[i],c1[i],c2,c3,c4)-ind.rpd5(x=roots[i],c1[i],c2,c3,c4))*n[i]
+      }else{
+        inte[i] = (ind.rpd5(x=roots[i],c1[i],c2,c3,c4)-ind.rpd5(x=brts_im1[i],c1[i],c2,c3,c4))*n[i]
+      }
+    }else{
+      inte[i] = (ind.rpd5(x=brts_i[i],c1[i],c2,c3,c4)-ind.rpd5(x=brts_im1[i],c1[i],c2,c3,c4))*n[i]
+    }
+  }
+  return(inte)
+}
+
+ind.rpd5 <- function(x,c1,c2,c3,c4){
+  r = (c2 * x^2)/2 + c1*x - (c3*exp(c4*x)*(c2*(c4*x-1)+c1*c4))/(c4^2) 
+  return(r)
+}
+
+intensity.rpd5c <- function(tree,pars){
+  n = tree$n; Pt = c(0,tree$pd)
+  c2 = pars[4]*((n-1)/n); c3 = exp(-pars[1]*max(tree$brts)); c4 = pars[1]
+  c1 = pars[2] + pars[3]*n + pars[4]*((Pt[-nrow(tree)]-n*c(0,tree$brts[-length(tree$brts)]))/n)
+  
+  roots = -c1/c2
+
+  brts_i = tree$brts
+  brts_im1 = c(0,brts_i[-length(brts_i)])
+  inte = vector(mode="numeric",length = length(brts_i))
+  for(i in 1:nrow(tree)){
+    if((roots[i] > brts_im1[i]) & (roots[i] < brts_i[i])  ){
+      if((pars[2] + pars[3] * n[i] + pars[4] *(Pt[i]/n[i]))<0){
+        inte[i] = (ind.rpd5(x=brts_i[i],c1[i],c2[i],c3,c4)-ind.rpd5(x=roots[i],c1[i],c2[i],c3,c4))*n[i]
+      }else{
+        inte[i] = (ind.rpd5(x=roots[i],c1[i],c2[i],c3,c4)-ind.rpd5(x=brts_im1[i],c1[i],c2[i],c3,c4))*n[i]
+      }
+    }else{
+      inte[i] = (ind.rpd5(x=brts_i[i],c1[i],c2[i],c3,c4)-ind.rpd5(x=brts_im1[i],c1[i],c2[i],c3,c4))*n[i]
+    }
+  }
+  return(inte)
+}
+
+
